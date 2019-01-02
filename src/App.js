d2547ca16f32b03f79e05d3b4029d48ead79dddb
jv <ljw@live.jp>
2019-01-02 20:58:33 +0800

    refactor: 🔥 e-learning

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React',
+    };
+
+    this.alert = this.alert.bind(this);
+  }
+
+  say() {
+    return `${this.props.hello} ${this.state.name}!`;
+  }
+
+  alert() {
+    // eslint-disable-next-line
+    alert(this.say());
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header" onClick={this.alert}>
+          {this.say()}
         </header>
       </div>
     );
   }
 }
+
+App.defaultProps = {
+  hello: 'Hello',
+};
+
+App.propTypes = {
+  hello: PropTypes.string,
+};
 
 export default App;
