1b70a0e115060567c3091d7f2aaa8d88a7db958d
jv <ljw@live.jp>
2019-01-06 18:20:32 +0800

    refactor: 🔥 reload

---
+++ src/router.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+// import { Router, Route, hashHistory } from 'react-router';
+
+export default class App extends Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
